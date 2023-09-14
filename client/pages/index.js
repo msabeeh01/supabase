a901988ceb295a9acce8b34bdaac555b8daf7eda
Muhammad Sabeeh <muhammadsabeeh01@gmail.com>
2023-09-14 14:03:31 -0400

    multiple images load

--- client/pages/index.js
+++ client/pages/index.js
@@ -9,35 +9,9 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [data, setData] = useState(null)
-  const [imageSrc, setImageSrc] = useState(null)
-  const [images, setImages] = useState([])
+  const [allImages, setAllImages] = useState([])
 
   useEffect(() => {
-    const handleFetch = async () => {
-      const { data, error } = await supabase
-        .from('test')
-        .select()
-        .eq('id', 1)
-      console.log(data)
-      setData(data)
-    }
-
-    const handleImageFetch = async () => {
-      const { data, error } = await supabase
-        .storage
-        .from('images')
-        .list('people', {
-          limit: 100,
-          offset: 0
-        })
-      if (error) {
-        console.log(error)
-      } else {
-        setImages(data)
-      }
-
-    }
-
     handleImageFetch()
   }, [])
 
@@ -51,19 +25,44 @@ export default function Home() {
       window.location.reload()
     }
   }
-
-  const createImage = async (name) => {
-    const { data, error } = await supabase.storage
-      .from('images/people')
-      .createSignedUrl(name, 60)
+  const handleImageFetch = async () => {
+    const { data, error } = await supabase
+      .storage
+      .from('images')
+      .list('people', {
+        limit: 100,
+        offset: 0
+      })
     if (error) {
       console.log(error)
     } else {
-      console.log(data.signedUrl)
-      return data.signedURL
+      const signedUrlPromises = data.map(async (image) => {
+        const { data, error } = await supabase
+          .storage
+          .from('images/people')
+          .createSignedUrl(image.name, 60)
+        if (error) {
+          console.log(error)
+          return null
+        }
+        return data.signedUrl
+      })
+
+      const signedUrls = await Promise.all(signedUrlPromises)
+      setAllImages(signedUrls.filter(url => url !== null))
+
     }
 
   }
+  const handleFetch = async () => {
+    const { data, error } = await supabase
+      .from('test')
+      .select()
+      .eq('id', 1)
+    console.log(data)
+    setData(data)
+  }
+
 
 
 
@@ -75,13 +74,10 @@ export default function Home() {
       <div style={{ display: "flex", backgroundColor: "blue", flexDirection: "column" }}>
         <input type='file' onChange={(e) => handleUpload(e.target.files[0])} />
 
-        <button onClick={() => console.log(images)}>Click</button>
-
-        {images && images.map((image) => {
-          const src = createImage(image.name)
+        {allImages && allImages.map((image, i) => {
           return (
-            <div key={image.name}>
-              <img src={src} alt='image' />
+            <div key={i}>
+              <img src={image} width={100} height={100} />
             </div>
           )
         })}
