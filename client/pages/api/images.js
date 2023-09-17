c6ddb9ebc900a73d1a3979b3c719ce2edd13a98d
Muhammad Sabeeh <muhammadsabeeh01@gmail.com>
2023-09-17 02:10:47 -0400

    optimization

--- client/pages/api/images.js
+++ client/pages/api/images.js
@@ -6,34 +6,48 @@ export default async function handler(req, res) {
         //req.body
         const userID = req.body.userID
 
-        //redis.keys('*').then((keys) => { res.status(200).json(keys) });
+        //generate a cache key baesd on the userID
+        const cacheKey = `images:${userID}`
 
-        const { data, error } = await supabase
-            .storage
-            .from('images')
-            .list(`${userID}`, {
-                limit: 100,
-                offset: 0
-            })
-        if (error) {
-            console.log(error)
+        //get images from cache
+        const cachedImages = await redis.get(cacheKey)
+
+        if (cachedImages) {
+            const images = JSON.parse(cachedImages)
+            return res.status(200).json(images)
         } else {
-            const signedUrlPromises = data.map(async (image) => {
-                const { data, error } = await supabase
-                    .storage
-                    .from(`images`)
-                    .createSignedUrl(`${userID}/${image.name}`, 60)
-                if (error) {
-                    console.log(error)
-                } else {
-                    return data
-                }
-            })
+            const { data, error } = await supabase
+                .storage
+                .from('images')
+                .list(`${userID}`, {
+                    limit: 100,
+                    offset: 0
+                })
+            if (error) {
+                console.log(error)
+                return res.status(500).json({ error: 'Failed to fetch images' })
+            } else {
+                const signedUrlPromises = data.map(async (image) => {
+                    const { data, error } = await supabase
+                        .storage
+                        .from(`images`)
+                        .createSignedUrl(`${userID}/${image.name}`, 60)
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        return data
+                    }
+                })
 
-            Promise.all(signedUrlPromises).then((values) => {
-                res.status(200).json(values)
-            })
+                const images = await Promise.all(signedUrlPromises)
+
+                await redis.set(cacheKey, JSON.stringify(images), 'EX', 59)
+
+                return res.status(200).json(images)
+            }
         }
+
+
     } else {
         res.status(405).json({ message: 'Method not allowed' })
     }
